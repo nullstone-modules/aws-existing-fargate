964c17515995907e416f885a43b1c64725cf9b74
Brad Sickles <brad.sickles@gmail.com>
2021-03-03 11:14:14 -0500

    Adding execution role

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "cluster_name" {
   value       = data.aws_ecs_cluster.this.name
   description = "string ||| "
 }
+
+output "cluster_execution_role_name" {
+  value       = aws_iam_role.execution.name
+  description = "string ||| "
+}
